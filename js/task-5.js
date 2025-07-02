5796d76ba582588700fabe9473c97586cce93e4a
tokareva-stacy <tokarevanyura@gmail.com>
2025-07-02 14:06:21 +0300

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
+const changeColorButton = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const bodyElement = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+changeColorButton.addEventListener("click", () => {
+    const randomColor = getRandomHexColor();
+
+    bodyElement.style.backgroundColor = randomColor;
+
+    colorSpan.textContent = randomColor;
+});
