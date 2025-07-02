40fdc4be417d140d086d9915d855d8cb7af376a6
tokareva-stacy <tokarevanyura@gmail.com>
2025-07-02 13:40:41 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
 
+// 2. Добавляем слушателя события 'submit'
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+
+    if (emailValue === "" || passwordValue === "") {
+        alert("All form fields must be filled in");
+    } else {
+        const formData = {
+            email: emailValue,
+            password: passwordValue,
+        };
+
+        console.log(formData);
+
+        loginForm.reset();
+    }
+});
